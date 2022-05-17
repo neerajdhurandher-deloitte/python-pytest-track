810a5b6afa1e504d511c812d5983e9fe7b9899ba
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2022-05-17 14:34:41 +0530

    problem 2 done

--- assignment_1/solution.py
+++ assignment_1/solution.py
@@ -18,4 +18,26 @@ class StringClass:
 
 stringClass = StringClass("hello")
 print("length of the string :- ", stringClass.lengthOfString())
-print("String converted to list : - ", stringClass.str2listConverter("neeraj"))
+print("String converted to list : - ", stringClass.str2listConverter("HashedIn"))
+
+
+# Problem 2
+# Create class PairsPossible which should inherit from StringClass
+# and should have a method for storing list of all possible pairs formed.
+# It should also have a method to print list of every possible pair in
+# same line but with space between them. Pairs should be in list.
+
+class PairsPossible(StringClass):
+    def pairs(self):
+        list = []
+
+        list1 = StringClass.str2listConverter(self,self.string)
+        for i in range(0,len(list1)):
+            temp = ""
+            for j in range(i+1,len(list1)):
+                temp = list1[i]+list1[j]
+                list.append(temp)
+        return list
+
+pairPossible = PairsPossible("hello")
+print("all possible pairs are :- ", pairPossible.pairs())
