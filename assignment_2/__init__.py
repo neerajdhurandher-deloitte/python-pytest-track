70534549d6125fbbe3341b79818bf017d7efc8c1
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2022-05-20 01:41:48 +0530

    Assignment 2 :- problem 3 & 4 done

--- assignment_2/__init__.py
+++ assignment_2/__init__.py
@@ -16,6 +16,7 @@ def get_duplicates(input_list):
 
     return duplicates
 
+
 # numbers = [1, 2, 3, 2, 5, 3, 3, 5, 6, 3, 4, 5, 7]
 # duplicates_items = get_duplicates(numbers)
 # print("Duplicates Items :-", duplicates_items)
@@ -30,8 +31,40 @@ def merge_list(list1, list2):
         new_list.append(new_item)
     return new_list
 
+
 # list1 = ["a", "c"]
 # list2 = ["b", "d"]
 # mergedList = merge_list(list1, list2)
 # print("Merged list is  :-", mergedList)
 
+# question 3
+# Given a nested list extend it by adding the
+# sub list ["h", "i", "j"] in such a way that it
+# will look like the following list
+def add_in_nested_list():
+    given_list = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+    add = ["h", "i", "j"]
+    given_list[2][1][2].extend(add)
+    print(given_list)
+
+
+# add_in_nested_list()
+
+# question 4
+# Map the dictionary in the following manner
+# Keys = [‘Ten’, ‘Twenty’, ‘Thirty’]
+# Value = [10,20,30]
+
+def create_map_ascending(key, value):
+    # assume that length of both lists are equal
+    dict = {}
+
+    for index in range(len(key)):
+        mapped = map(key[index], value[index])
+        dict.update(mapped)
+
+    print(dict)
+
+# keys = ['Ten', 'Twenty', 'Thirty']
+# value = [10, 20, 30]
+# create_map_ascending(keys, value)
